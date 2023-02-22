775727ce7bb6b1cb86146431f69df663268f4ab4
Rob Mensching <rob@firegiant.com>
2023-02-22 12:39:42 -0800

    Initial repo with build and app ready to go

---
+++ build.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+pushd %~dp0
+
+echo Initializing dev environment
+
+for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version [17.0^,18.0^) -property installationPath`) do (
+  call "%%i\Common7\Tools\vsdevcmd.bat" -no_logo
+)
+
+echo Build
+
+msbuild -nologo -m BeltTest\BeltTest.sln -p:BuildVersion=1.0
