0c433af84acefd84eef94785779d684d68b8a3d0
Rob Mensching <rob@firegiant.com>
2023-03-29 11:49:03 -0700

    Add Deployment Dojo Counting Service

---
+++ BeltTest/WindowsService1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceProcess;
+
+namespace WindowsService1
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            ServiceBase.Run(new Service1());
+        }
+    }
+}
