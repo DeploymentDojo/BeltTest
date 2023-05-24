9da41fcfc64c4a13b1ebd08510b8b03ef7a15b54
Rob Mensching <rob@firegiant.com>
2023-05-24 14:55:47 -0700

    Add restore step to official build

--- build.cmd
+++ build.cmd
@@ -10,4 +10,6 @@ for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\
 
 echo Build
 
+nuget restore BeltTest\BeltTest.sln
+
 msbuild -nologo -m BeltTest\BeltTest.sln -p:BuildVersion=0.33
